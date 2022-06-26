510d4f05306ad8550b16e33e2e770e82fcff3375
Arsene Awounou <developer.awounou@gmail.com>
2022-06-26 20:02:27 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,11 +6,12 @@ def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
     if (type(boxes) is not list or len(boxes) == 0):
         return False
-    unlocked = [0]
-    for key in unlocked:
-        for val in boxes[key]:
-            if val not in unlocked:
-                unlocked.append(val)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
