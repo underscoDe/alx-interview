c914c6eec5e4ac8ed994e2f6a8d7214dbe64a77d
Arsene Awounou <developer.awounou@gmail.com>
2022-07-07 14:35:45 +0100

    import

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Stats """
-from sys import stdin
 
 
 def print_log_stats(file_size, stats):
@@ -11,7 +10,8 @@ def print_log_stats(file_size, stats):
 
 
 if __name__ == '__main__':
-    """Reads stdin line by line and computes metrics"""
+    from sys import stdin
+
     count = 0
     total_size = 0
     status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
