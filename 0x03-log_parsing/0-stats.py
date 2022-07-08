f9f14ef9bbf664c612b76bab6dc63d5bcff95620
Arsene Awounou <developer.awounou@gmail.com>
2022-07-08 07:54:35 +0100

    doc

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Log Parsing
-"""
+"""Log Parsing"""
 from sys import stdin
 
 
@@ -13,6 +11,7 @@ def print_log_stats(file_size, stats):
 
 
 if __name__ == '__main__':
+
     count = 0
     total_size = 0
     status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
