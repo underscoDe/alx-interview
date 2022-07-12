1f6944d94346e94df5f31a147942c8f4b90850cd
Arsene Awounou <developer.awounou@gmail.com>
2022-07-12 19:19:52 +0100

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    helper1 = 1 << 7
+    helper2 = 1 << 6
+    bits_count = 0
+
+    for i in range(len(data)):
+        temp_helper = 1 << 7
+        while temp_helper & data[i]:
+            bits_count += 1
+            temp_helper >>= 1
+
+        if bits_count > 4:
+            return False
+
+        for j in range(1, bits_count):
+            if i + j >= len(data):
+                return False
+            if not (data[i + j] & helper1 and not (data[i + j] & helper2)):
+                return False, '10'
+        i += bits_count - 1
+    return True
